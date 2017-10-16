84d4a91634c296ef6880be25eb1de3ab45027d47
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-16 16:11:02 -0700

    Created Main and Hello World and template for future programs

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**************************************************************************
+ * AUTHOR  : Cameron W
+ * PROJECT : Java Practice
+ *************************************************************************/
+
+public class Main {
+	public static void main(String[] args) {
+		/****************************************************************************
+		 * 							Testing Programs
+		 ****************************************************************************/
+		System.out.println("\n\n\t\tTesting Programs");
+		System.out.println("\n******************************************************************");
+
+		System.out.println("\n\n\t Hello World Testing Program");
+		System.out.println("\n------------------------------------------------------------------");
+		System.out.println("Hello from Java!");
+	}
+
+}
