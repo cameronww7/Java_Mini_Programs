832a43baa7c602c3c61c38efdb72eb9cf6b457f7
cam.w.w <cameronww7@users.noreply.github.com>
2017-10-16 17:45:09 -0700

    Hello world Sub

---
+++ Exploring_Java/HelloWorld.java
@@ -0,0 +1,12 @@
+
+package Exploring_Java;
+
+import com.example.java.model.Olive;
+
+import java.util.List;
+
+public class HelloWorld {
+	public static void helloWorld(void) {
+		System.out.println("> Hello from Java!");
+	}
+}
